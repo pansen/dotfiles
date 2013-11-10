e4fe32b4ef73f19842376e7b6efaff9b5a90e733
andi <andi@inventorum.com>
2013-11-10 16:40:41 +0100

    - add ``tree`` to dependency packages

--- macports.sh
+++ macports.sh
@@ -2,7 +2,8 @@
 port -v install ffmpeg +nonfree 
 port -v install mplayer +smb +mencoder_extras +a52 +aa +noappleremote +osd
 port -v install git-core +bash_completion +credential_osxkeychain +svn
-port -v install coreutils bash bash-completion nmap ngrep htop vnstat apple-gcc42 cmake apg
+port -v install coreutils bash bash-completion nmap ngrep htop vnstat apple-gcc42 \
+    cmake apg tree
 port -v install vim +python27
 
 echo "sudo vim /etc/shells"
