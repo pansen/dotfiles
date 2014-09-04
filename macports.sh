3a5c0455fa51c8809aab86a87f8fcecf71268646
andi <andi@inventorum.com>
2014-09-04 08:47:02 +0200

    - install unix2dos by default

--- macports.sh
+++ macports.sh
@@ -11,6 +11,7 @@ port -v install apple-gcc42 cmake gnupg
 port -v install ffmpeg +nonfree 
 port -v install mplayer +smb +mencoder_extras +a52 +aa +noappleremote +osd
 port -v install testdisk
+port -v install dos2unix unix2dos
 
 echo "sudo vim /etc/shells"
 echo "chsh -s /opt/local/bin/bash $USER"
