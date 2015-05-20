311f15da744e62e7a011036c7c244f0588f4f6a1
andi <andi@inventorum.com>
2015-05-20 14:04:40 +0200

    - more packages

--- macports/macports.sh
+++ macports/macports.sh
@@ -8,13 +8,15 @@ port -v install py27-docutils
 port -v install vim +python27
 port -v install git +bash_completion +svn -credential_osxkeychain
 port -v install the_silver_searcher
-port -v install nmap ngrep htop vnstat iftop bmon libyaml ImageMagick
+port -v install nmap ngrep htop vnstat iftop bmon libyaml ImageMagick lnav
 port -v install apple-gcc42 cmake gnupg
 port -v install ffmpeg +nonfree 
 port -v install mplayer +smb +mencoder_extras +a52 +aa +noappleremote +osd
 port -v install testdisk
 port -v install dos2unix 
 port -v install go
+port -v install pandoc
+port -v install mutt +compress
 
 echo "sudo vim /etc/shells"
 echo "chsh -s /opt/local/bin/bash $USER"
