a033a331c13df0bf09cdccae91f11e67235e22ad
andi <andi@inventorum.com>
2015-06-29 17:01:50 +0200

    - adjust packages to yosemite and add a few

--- macports/macports.sh
+++ macports/macports.sh
@@ -9,13 +9,13 @@ port -v install vim +python27
 port -v install git +bash_completion +svn -credential_osxkeychain
 port -v install the_silver_searcher
 port -v install nmap ngrep htop vnstat iftop bmon libyaml ImageMagick lnav
-port -v install apple-gcc42 cmake gnupg
+port -v install cmake gnupg
 port -v install ffmpeg +nonfree 
 port -v install mplayer +smb +mencoder_extras +a52 +aa +noappleremote +osd
 port -v install testdisk
 port -v install dos2unix 
 port -v install go
-port -v install pandoc
+port -v install pandoc xmlstarlet jq
 port -v install mutt +compress
 
 echo "sudo vim /etc/shells"
