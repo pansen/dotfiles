23fed18b4f57dce562c7db39b2498eb3fe632868
andi <andi@inventorum.com>
2015-07-16 13:28:34 +0200

    - ssh key generation command

--- macports/macports.sh
+++ macports/macports.sh
@@ -10,6 +10,7 @@ port -v install git +bash_completion +svn -credential_osxkeychain
 port -v install the_silver_searcher
 port -v install nmap ngrep htop vnstat iftop bmon libyaml ImageMagick lnav
 port -v install cmake gnupg
+port -v install openssh
 port -v install ffmpeg +nonfree 
 port -v install mplayer +smb +mencoder_extras +a52 +aa +noappleremote +osd
 port -v install testdisk
@@ -20,3 +21,7 @@ port -v install mutt +compress
 
 echo "sudo vim /etc/shells"
 echo "chsh -s /opt/local/bin/bash $USER"
+
+# https://stribika.github.io/2015/01/04/secure-secure-shell.html
+echo "did you generate a ``ed25519`` ssh key yet?"
+echo "/opt/local/bin/ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519"
